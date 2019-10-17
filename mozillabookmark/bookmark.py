c34301bb89cab49eb0a9b46f04ffc94e381d3780
llk23r <mail.acharyarahul.now@gmail.com>
2019-10-18 01:07:06 +0530

    Fix for Firefox and Mac

--- mozillabookmark/bookmark.py
+++ mozillabookmark/bookmark.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 import os
+import platform
 
 
 class BookMarkLocation(object):
     def __init__(self):
-        self.firefox_bookmark_location = "{}/Library/Application Support/Firefox".format(
-            os.environ['HOME'])
+        home_path = os.environ["HOME"]
+        if(platform.system() == 'Linux'):
+            self.firefox_bookmark_location = "{}/.mozilla/firefox/".format(
+                home_path)
+        elif(platform.system() == 'Darwin'):
+            self.firefox_bookmark_location = "{}/Library/Application Support/Firefox".format(
+                home_path)
 
     def get_bookmark_location(self):
         for root, dirs, files in os.walk(self.firefox_bookmark_location):
